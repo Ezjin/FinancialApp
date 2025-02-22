6347becfaf315851c0b5693e779d41bd60d95f74
Ezjin <pmartins@duck.com>
2025-02-22 10:37:28 -0300

    adicionado mensagens de error que ajudem a completar as informacoes da nota

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -9,22 +9,45 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Por favor selecione um cliente.'
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, {message: 'Por favor selecione um valor maior que R$0,00.'}),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Por favor selecione um status.'    
+    }), 
     date: z.string(),
-})
+});
 
 const CreateInvoice = FormSchema.omit({id: true, date: true})
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+    errors?: {
+      customerId?: string[];
+      amount?: string[];
+      status?: string[];
+    };
+    message?: string | null;
+  };
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
 
+    if (!validatedFields.success){
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Campos faltando. Falha em criar a nota.'
+        };
+    }
+    
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
 
@@ -34,8 +57,9 @@ export async function createInvoice(formData: FormData) {
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
         `;
     } catch (error) {
-        // Printa o erro no console
-        console.error(error);
+        return{
+            message: 'Database error: Falha em criar a nota.'
+        }
     }
     
     revalidatePath('/dashboard/invoices');
